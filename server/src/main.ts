0ab4bb553ba3615dd9d291c2f24ee0fc1b398bae
Yuri Nemishev <ynemishev@wrshealth.com>
2025-06-17 16:10:27 +0300

    Added optimized Dockerfile for the server, added nginx reverse-proxy, added redis

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const SERVER_PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableShutdownHooks(); // Tells NestJS to handle shutdown signals
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
